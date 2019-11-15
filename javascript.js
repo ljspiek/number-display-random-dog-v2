1a48011f811360238b20256d9737249c9d0d0f59
Laura Spieker <ljspiek@gmail.com>
2019-11-15 12:02:21 -0600

    updated js to display dog images

--- javascript.js
+++ javascript.js
@@ -9,12 +9,26 @@ function getDogImage() {
   fetch(STORE.numURL)
     .then(response => response.json())
     .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+      displayResults(responseJson));
+    // .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
+  let doggoUrls = responseJson.message;
+  console.log(doggoUrls);
+  let ul = $("#dog-images");
+  ul.empty();
+  doggoUrls.forEach(img => {
+    ul.append(`<li><img src="${img}"/></li>`);
+  });
+  $("section").show();
+ 
+  // $('.results-img').replaceWith(
+  //   `<img src="${responseJson.message}" class="results-img">`
+  // )
+  // //display the results section
+  // $('.results').removeClass('hidden');
   }
 
 function watchForm() {
